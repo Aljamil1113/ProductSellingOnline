20852c9d17f2dd9b1abd6b91dd4153630c38e308
Aljamil1113 <aarazasaljamil@gmail.com>
2020-03-30 22:58:58 +0800

    CRUD ProductTypes

--- ProductSellingOnline/Areas/Admin/Controllers/ProductTypeController.cs
+++ ProductSellingOnline/Areas/Admin/Controllers/ProductTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProductSellingOnline.Data;
+using ProductSellingOnline.Models;
 
 namespace ProductSellingOnline.Areas.Admin.Controllers
 {
@@ -22,7 +23,105 @@ namespace ProductSellingOnline.Areas.Admin.Controllers
             return View(db.ProductTypes.ToList());
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductType prodTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                db.ProductTypes.Add(prodTypes);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index", "ProductType");
+            }
+
+            return View(prodTypes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+
+            }
+            ProductType Ptype = await db.ProductTypes.FindAsync(id);
+
+            if(Ptype == null)
+            {
+                return NotFound();
+            }
+            return View(Ptype);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductType productType)
+        {
+            if(id != productType.Id)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                db.ProductTypes.Update(productType);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index", "ProductType");
+            }
+
+            return View(productType);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var PType = await db.ProductTypes.FindAsync(id);
+
+            if(PType == null)
+            {
+                return NotFound();
+            }
+
+            return View(PType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productType = await db.ProductTypes.FindAsync(id);
+            db.ProductTypes.Remove(productType);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", "ProductType");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await db.ProductTypes.FindAsync(id);
+
+            if(productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
     }
 }

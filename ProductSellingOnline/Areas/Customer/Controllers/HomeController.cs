d5cf15acf1a4a0c7496cf624b62c89c9b755a0cb
Aljamil1113 <aarazasaljamil@gmail.com>
2020-04-27 23:24:20 +0800

    Home page

--- ProductSellingOnline/Areas/Customer/Controllers/HomeController.cs
+++ ProductSellingOnline/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductSellingOnline.Data;
 using ProductSellingOnline.Models;
 
 namespace ProductSellingOnline.Areas.Customer.Controllers
@@ -11,9 +13,34 @@ namespace ProductSellingOnline.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext db;
+
+        public HomeController(ApplicationDbContext _db)
         {
-            return View();
+            db = _db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var productList = await db.Products.Include(p => p.ProductType).Include(s => s.SpecialTag).ToListAsync();
+            return View(productList);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? Id)
+        {
+            if(Id == null)
+            {
+                return NotFound();
+            }
+
+            var product = db.Products.Include(p => p.ProductType).Include(s => s.SpecialTag).SingleOrDefault(x => x.Id == Id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
 
         public IActionResult About()

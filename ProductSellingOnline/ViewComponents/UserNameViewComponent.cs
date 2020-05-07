6337423ce5eb92cc8e1a9a789bc2df8ca23a1708
Aljamil1113 <aarazasaljamil@gmail.com>
2020-05-07 17:41:06 +0800

    ViewComponent

---
+++ ProductSellingOnline/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductSellingOnline.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ProductSellingOnline.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+
+        public UserNameViewComponent(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = db.ApplicationUser.Where(u => u.Id == claims.Value).FirstOrDefault();
+
+            return View(userFromDb);
+        }
+    }
+}
